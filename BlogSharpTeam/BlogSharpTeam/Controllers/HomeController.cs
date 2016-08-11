9ba0fded9e85e63339814f9cb56ebfcc881878f6
vessos <vinsent_vsm@abv.bg>
2016-08-11 20:01:47 +0300

    Home-Page
    Changed -updated -1st version

--- BlogSharpTeam/BlogSharpTeam/Controllers/HomeController.cs
+++ BlogSharpTeam/BlogSharpTeam/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace BlogSharpTeam.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Fun()
         {
             ViewBag.Message = "Your application description page.";
 
