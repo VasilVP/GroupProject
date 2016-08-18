72b06024bd1ae4abb861670f8426d949fe62d328
vessos <vinsent_vsm@abv.bg>
2016-08-18 17:54:15 +0300

    Home-LastPostView
    Last post view in home page

--- BlogSharpTeam/BlogSharpTeam/Controllers/HomeController.cs
+++ BlogSharpTeam/BlogSharpTeam/Controllers/HomeController.cs
@@ -3,16 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using BlogSharpTeam.Models;
 
 namespace BlogSharpTeam.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
+       
         public ActionResult FunIndex()
         {
             ViewBag.Message = "Your application description page.";
@@ -38,6 +36,13 @@ namespace BlogSharpTeam.Controllers
             ViewBag.Message = "Your posts page.";
 
             return View();
+        }
+        private ApplicationDbContext db = new ApplicationDbContext();
+        //Get: Posts
+        public ActionResult Index()
+        {
+            var posts = db.Posts.Include(p => p.Author).OrderByDescending(p => p.Date).Take(3);
+            return View(posts.ToList());
         }
     }
 }
